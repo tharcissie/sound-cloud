359663a8058be19e861075dc4c3cdabd7f4dc0cc
tharcissie <tharcissieidufashe@gmail.com>
2023-02-03 12:52:36 +0200

    added sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatInputModule} from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BodyComponent } from './body/body.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomepageComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule
+    MatIconModule,
+    MatDividerModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sidebar/nav-data.ts
@@ -0,0 +1,12 @@
+export const navbarData = [
+    {
+        routeLink: 'home',
+        icon: 'view_module',
+        label: 'Your Library'
+    },
+    {
+        routeLink: 'playlist',
+        icon: 'playlist_play',
+        label: 'Playlist'
+    },
+];
